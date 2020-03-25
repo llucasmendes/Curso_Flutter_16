0ddc6aed8e40981fdda8703c34c376ca0d1139ed
Lucas <llucasmendes@hotmail.com>
2020-03-25 14:44:50 -0300

    Dicee + corona_br

---
+++ corona_br/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flame/util.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+
+void main() async {
+  Util flameUtil = Util();
+  await flameUtil.fullScreen();
+  await flameUtil.setOrientation(DeviceOrientation.portraitUp);
+
+  GameController gameController = GameController();
+  runApp(gameController.widget);
+}
---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.blueGrey,
+          appBar: AppBar(
+            title: Text('Corona BR'),
+            backgroundColor: Colors.blueGrey[900],
+          ),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/coronav.png'),
+            ),
+          ),
+        ),
+      ),
+    );
